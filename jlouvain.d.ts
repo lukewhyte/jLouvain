6727a07ea7316710f15c8f30e2a1f9fe77476b52
Luke Whyte <lukeawhyte@gmail.com>
2024-09-30 11:57:49 -0500

    Added parition_init to types

--- jlouvain.d.ts
+++ jlouvain.d.ts
@@ -4,6 +4,10 @@ export interface Edge {
   weight?: number;
 }
 
+export interface Partition {
+  [id: string]: number
+}
+
 export interface Communities {
   [id: string]: number; 
 }
@@ -12,6 +16,7 @@ export interface jLouvainGenerator {
   (): Communities;
   nodes: (nodes: string[]) => jLouvainGenerator;
   edges: (edges: Edge[]) => jLouvainGenerator;
+  partition_init: (parts: Partition) => jLouvainGenerator
 }
 
 export function jLouvain(): jLouvainGenerator;
